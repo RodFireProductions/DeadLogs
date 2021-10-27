32748e08dd5f27f7bec5c3f358eaea8fe387536a
RodFireProductions <53588604+RodFireProductions@users.noreply.github.com>
2021-10-27 15:27:08 -0500

    bah

--- script.js
+++ script.js
@@ -9,13 +9,11 @@
 -- Table of Contents --
   1. Basic Configs
   2. Posts
-  3.
-  4.
 */
 
 //=// --- Basic Configs --- //=// [1]
 
-let siteName = "My Blog!";
+let siteName = "DeadJournals";
 let lang = "en"; // The language of your site. Example, en = english, fr = français (french), etc.
 
 // Feel free to leave one of these values as 'null' if you don't wish to use them.
@@ -29,19 +27,19 @@ let socials = {
 
 // Feel free to leave one of these values as 'null' if you don't wish to use them.
 let author = {
-    name: "Your name",
-    title: 'A rock on the web', // A job title or personal title, what ever defines you <3
+    name: "Rodrick 'RodFireProductions'",
+    title: 'Just another dead inside artist.', // A job title or personal title, what ever defines you <3
     picture: "./assets/images/author.png", // This is the url of the author picture, can be local or a link. Square or almost square is recommended
-    pronouns: 'fae/faer', // Examples: he/him, she/her, they/them, ze/zir, etc. Resource: https://en.pronouns.page/
-    extra: ['aroace','donut stone', 'mixed'], // Your orientation, ethinicty, etc? If none, then set this to `null`
+    pronouns: 'they/them', // Examples: he/him, she/her, they/them, ze/zir, etc. Resource: https://en.pronouns.page/
+    extra: null, // Your orientation, ethinicty, etc? If none, then set this to `null`
 
     line: "Mushrooms are cute!" // A quote? A goofy phrase? Your own personal motto? Be creative!
 }
 
 let navi = {
     home: ['home','/index.html'],
-    about: ['examples','examples/index.html'],
-    archive: ['tutorial','/tutorial.html']
+    about: ['examples','/examples.html'],
+    archive: ['get started','/tutorial.html']
 }
 
 // --------------------------- # # # # #
@@ -58,19 +56,13 @@ let navi = {
     together and in chronological order.
 */
 let postArchive = [
-    "post/oct_25_2021_example.html",
-    "post/oct_26_2021_example2.html",
-    "post/oct_26_2021_example3.html"
+    //"post/oct_25_2021_example.html"
 ];
 let postArchiveNames = [
-    "Example Polar Post",
-    "Example Yet Post",
-    "Example Dog Post"
+    //"Example Polar Post"
 ];
 let postArchiveDates = [
-    "25 Oct 2021",
-    "26 Oct 2021",
-    "26 Oct 2021"
+    //"25 Oct 2021"
 ];
 
 // --------------------------- # # # # #
